d056684fac20f39285e0cbb9dd6b0d6c6154da7a
Lagnesh Thakur <lagneshthakur@gmail.com>
2021-05-29 10:06:14 +0530

    javascript basics session 2

--- hello.js
+++ hello.js
@@ -1,11 +1,43 @@
 console.log("Hello from Javascript!");
 console.log(this);
-
-// events
-
 // array methods
+// forEach, map, filter and reduce
+const mock_data = require('./MOCK_DATA.json');
 
-// loops
+let strengths = [];  
+mock_data.forEach((person) => {
+    strengths.push(person.strength);
+});
+
+let mock_data_mapped = mock_data.map((person) => {
+    return {
+        full_name: `${person.first_name} ${person.last_name}`,
+        country: person.country
+    }
+});
+// console.log(mock_data_mapped[56]);
+
+let mock_data_filtered = mock_data.filter((person) => {
+    if(person.last_name) {
+        return false;
+    } else {
+        return true;
+    }
+})
+
+// console.log(mock_data_filtered.length);
+// console.log(mock_data_filtered[24].first_name);
+
+
+
+function reducerSum(total, currentValue) {
+    return total + currentValue;
+}
+console.log(strengths.length)
+console.log(strengths)
+totalStrength = strengths.reduce(reducerSum);
+console.log("Total Strength :", totalStrength)
+
 
 // hoisting
 
@@ -16,3 +48,7 @@ console.log(this);
 // closure
 
 // currying
+
+// callbacks
+
+// promises and async/await
